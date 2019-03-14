2a0c2a3c0a6bb409befdf1fc1cd556e1c88cadde
Pablo Cid <pabloagronomo@gmail.com>
2019-03-14 20:41:53 -0300

    mod16

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ device.on('connect', function () {
         encoding: 'JPEG',
         quality: 75,
         awb: false,
-        awbgains: '2,1.1'
+        awbgains: '3,0.1'
     }
     raspberryPiCamera.start(options, () => {
         device.publish('LED', JSON.stringify({ message: 'The camera is ready' }));
